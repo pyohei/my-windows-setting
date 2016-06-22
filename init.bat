a660b56dad4a9fbef539fc9144a313d6515ba398
shohei mukai <mukaishohei76@gmail.com>
2016-06-22 15:42:17 +0900

    Add cmd init program

---
+++ init.bat
@@ -0,0 +1,21 @@
+@echo off
+
+doskey ls=ls --color=auto --show-control-chars -N $*
+doskey grep=grep --color=auto $*
+doskey rm=rm -i $*
+doskey cp=cp -i $*
+doskey mv=mv -i $*
+rem doskey vi=vim $*
+doskey vim=C:\vim\vim.exe $*
+doskey gvim=C:\vim\gvim.exe $*
+
+
+rem --- Set loaded flag ---
+if "%CMD_LOADED%" neq "" goto :eof
+set CMD_LOADED=1
+
+rem --- Set path ---
+set GIT_EDITOR=C:\vim\vim.exe
+set PATH=%PATH%;C:\Program Files\Git\bin;C:\Program Files\Git\usr\bin
+
+cls
